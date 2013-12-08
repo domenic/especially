5cebb1495e35222168c9641beeb8c75bf70c2528
Domenic Denicola <domenic@domenicdenicola.com>
2013-12-08 04:07:00 -0500

    Appease JSHint.

--- well-known-symbols.js
+++ well-known-symbols.js
@@ -1,5 +1,8 @@
 "use strict";
 
+/*jshint newcap: false */
+/*global Symbol: false */
+
 // http://people.mozilla.org/~jorendorff/es6-draft.html#sec-well-known-symbols
 
 exports["@@create"] = Symbol("Symbol.create");
